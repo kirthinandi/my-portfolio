f84030c9a2337fc5e8a0d56f373ec0ea49a4521b
Kirthi Nandi <kirthi.nandi@gmail.com>
2023-04-28 17:20:58 -0700

    styled navbar, created logo which navigates back to landing page

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import MyResume from './Kirthi Nandi Resume.pdf';
+import flowerImg from './flower.png';
+import { useNavigate } from 'react-router-dom';
 
 
 export default function Home() {
+    const navigate = useNavigate();
 
     function handleClick()  {
         window.open("https://www.linkedin.com/in/kirthi-nandi/", "_blank");
@@ -21,15 +24,22 @@ export default function Home() {
         window.open(MyResume);
       }
 
+      function handleLogoClick() {
+        navigate('/');
+      }
+
     return (
         <div className="home">
-            <nav>
-                <Link to="/home">Home </Link>
-                <Link to="/about">About </Link>
-                <Link to="/projects">Projects </Link>
-                <Link to="/contact">Contact </Link>
+            <nav className="navBar">
+                <div className="logo">
+                  <img src={flowerImg} alt="logo" onClick={handleLogoClick}/>
+                  <h1 className="backToLP" onClick={handleLogoClick}>Kirthi Nandi</h1>
+                </div>
+                <Link to="/home" className="homeLink">Home </Link>
+                <Link to="/about" className="links">About </Link>
+                <Link to="/projects" className="links">Projects </Link>
+                <Link to="/contact" className="links">Contact </Link>
             </nav>
-
             <button onClick={openResume}>Resume</button>
             <button onClick={handleClick}>LinkedIn</button>
             <button onClick={handleGithub}>Github</button>
