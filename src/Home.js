f6f086fb5a8b1df5f0468f7a8be6efbee8dc8463
Kirthi Nandi <kirthi.nandi@gmail.com>
2023-05-02 16:35:22 -0700

    created dropdown navbar and styled it

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Link} from "react-router-dom";
 import MyResume from './Kirthi Nandi Resume.pdf';
 import flowerImg from './flower.png';
@@ -6,7 +6,12 @@ import { useNavigate } from 'react-router-dom';
 
 
 export default function Home() {
+    const [showMenu, setShowMenu] = useState(false);
     const navigate = useNavigate();
+
+    const handleMenuClick = () => {
+      setShowMenu(!showMenu);  /*everytime the navbar lines are clicked, it will set the show menu to the opposite of the original state of show menu (will set to true) */
+    }
 
     function handleClick()  {
         window.open("https://www.linkedin.com/in/kirthi-nandi/", "_blank");
@@ -30,15 +35,22 @@ export default function Home() {
 
     return (
         <div className="home">
-            <nav className="navBar">
+            <nav className="navbar">
+                <div className="navbar__menu-icon" onClick={handleMenuClick}>
+                  <div className="navbar__menu-icon__line"></div>
+                  <div className="navbar__menu-icon__line"></div>
+                  <div className="navbar__menu-icon__line"></div>
+                </div>    
                 <div className="logo">
                   <img src={flowerImg} alt="logo" onClick={handleLogoClick}/>
-                  <h1 className="backToLP" onClick={handleLogoClick}>Kirthi Nandi</h1>
+                  <h1 onClick={handleLogoClick}>Kirthi Nandi</h1>
                 </div>
-                <Link to="/home" className="homeLink">Home </Link>
-                <Link to="/about" className="links">About </Link>
-                <Link to="/projects" className="links">Projects </Link>
-                <Link to="/contact" className="links">Contact </Link>
+                <div className={`navbar__menu ${showMenu ? 'navbar__menu--show' : ''}`}>
+                  <Link to="/home" className="links">Home </Link>
+                  <Link to="/about" className="links">About </Link>
+                  <Link to="/projects" className="links">Projects </Link>
+                  <Link to="/contact" className="links">Contact </Link>
+                </div>
             </nav>
             <button onClick={openResume}>Resume</button>
             <button onClick={handleClick}>LinkedIn</button>
